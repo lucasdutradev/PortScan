75d7ea7ee0237311df1c419e9e2a2c1e8c476fef
Lucas Bryan <lucasbryan0217@gmail.com>
2025-09-27 16:00:36 -0400

    feat: add test case for host and ip validation

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -63,6 +63,28 @@ class TestCLIMethods(unittest.TestCase):
     result = app.cli.port_int(value_input)
     self.assertEqual(1000, result)
 
+  """
+    host_type test cases
+  """
+
+  def test_host_type_should_be_invalid_when_input_is_invalid_ip(self):
+    value_input = "10.10"
+    with self.assertRaises(ArgumentTypeError) as context:
+      app.cli.host_type(value_input)
+    self.assertEqual("should be literal valid ip or hostname", str(context.exception))
+
+  def test_host_type_should_be_valid_when_input_is_a_literal_ip(self):
+    value_input = "127.0.0.1"
+    result = app.cli.host_type(value_input)
+    self.assertEqual("127.0.0.1", result)
+
+  def test_host_type_should_be_valid_when_input_is_a_hostname(self):
+    value_input_a = "http://localhost:8000"
+    value_input_b = "https://localhost:8000"
+    result_a = app.cli.host_type(value_input_a)
+    result_b = app.cli.host_type(value_input_b)
+    self.assertEqual("http://localhost:8000", result_a)
+    self.assertEqual("https://localhost:8000", result_b)
 
 if __name__ == '__main__':
     unittest.main()
